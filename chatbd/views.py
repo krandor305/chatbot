addd549748e4f6e86ea0de460f7392bac0ca1def
krandor305 <al-had@hotmail.com>
2018-07-15 20:38:34 +0100

    chatbot

--- chatbd/views.py
+++ chatbd/views.py
@@ -3,10 +3,9 @@ from .forms import ChatForm
 from django.http import HttpResponseRedirect,HttpResponse
 
 def index(request):
-    message="vu"
     form=ChatForm()
-    if request.is_ajax() and request.POST:
-        message=request.POST.get('message','')
-    return render(request,'chatbd/index.html',{'form':form,'message':message})
+    return render(request,'chatbd/index.html',{'form':form})
 
-
+def envoi(request):
+    message = request.GET.get('message', None)
+    return HttpResponse(message)
